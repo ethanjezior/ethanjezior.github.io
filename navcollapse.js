5b2f438f2e6f53dc4c701a9310c1dc109528ec5d
Ethan <143633324+ethanjezior@users.noreply.github.com>
2023-12-15 13:58:03 -0500

    Navbar progress + Forward Fitness Club update

--- navcollapse.js
+++ navcollapse.js
@@ -1,25 +1,60 @@
 window.addEventListener('DOMContentLoaded', () => {
+    var nav = getElementsByTagName("nav");
+    var navUl = nav.getElementsByTagName("ul");
+    var navLi = navUl.getElementsByClassName("navLi");
+    var navA = navUl.getElementsByTagName("a");
+
+    var a = getElementsByClassName("navLi-One");
+    var b = getElementsByClassName("navLi-Two");
+    var c = getElementsByClassName("navLi-Three");
+    var d = getElementsByClassName("navLi-Four");
+    var e = getElementsByClassName("navLi-Five");
+
+    a.style.display = "none";
     document.getElementsByClassName("toggleNav").addEventListener("click", toggleNav);
     function toggleNav() {
-        var a = getElementsByClassName("navLi-One");
-        var b = getElementsByClassName("navLi-Two");
-        var c = getElementsByClassName("navLi-Three");
-        var d = getElementsByClassName("navLi-Four");
-        var e = getElementsByClassName("navLi-Five");
         if(a.style.display == "none") {
+            a.style.display = "block";
+            /*
             a.style.display = "block";
             b.style.display = "block";
             c.style.display = "block";
             d.style.display = "block";
             e.style.display = "block";
+            */
+            a.style.boxSizing = "border-box";
+            a.style.margin = "none";
+            navLi.style.padding = "none";
+            navLi.style.height = "10vh";
+            navLi.style.width = "100%";
+            navLi.style.fontFamily = "Verdana,Arial,sans-serif";
+            navLi.style.fontWeight = "bold";
+            navLi.style.listStyleType = "none";
+            navLi.style.borderBottom = "1px,solid,#ffffff";
+
+            navLi.style.boxSizing = "border-box";
+            navLi.style.margin = "none";
+            navLi.style.padding = "none";
+            navLi.style.height = "10vh";
+            navLi.style.width = "100%";
+            navLi.style.fontFamily = "Verdana,Arial,sans-serif";
+            navLi.style.fontWeight = "bold";
+            navLi.style.listStyleType = "none";
+            navLi.style.borderBottom = "1px,solid,#ffffff";
+
+            navA.style.color = "#ff99ff";
+            navA.style.fontSize = "5vh";
+            navA.style.textAlign = "center";
             
-            a.style.height = ""
         } else {
+            navLi.style.display = "none";
+            /*
             a.style.display = "none";
             b.style.display = "none";
             c.style.display = "none";
             d.style.display = "none";
             e.style.display = "none";
+            */
         }
     }
 })
