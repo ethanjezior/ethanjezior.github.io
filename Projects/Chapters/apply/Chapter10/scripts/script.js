b3cda18827b227f424c313a6d363d3c69cc60cf1
Ethan <143633324+ethanjezior@users.noreply.github.com>
2024-01-10 14:21:00 -0500

    Added Projects

---
+++ Projects/Chapters/apply/Chapter10/scripts/script.js
@@ -0,0 +1,14 @@
+/* 
+Ethan Jezior
+script.js
+1/10/24
+*/
+
+// function to display content.
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
+var b = document.getElementByTagName("button");
