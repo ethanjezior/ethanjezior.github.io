654e85dae736e2b78f8ce64cfe13618f8152141d
Ethan <143633324+ethanjezior@users.noreply.github.com>
2024-04-05 13:22:40 -0400

    JS Programs

---
+++ Projects/JS/project1-04/project01-04_txt.js
@@ -0,0 +1,14 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Ethan Jezior
+    Date:   04/5/24
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+homeStreet="1 Main St.",homeCity="sicilia",homeState="MA",
+homeCode="02103",workStreet="15 Oak Ln.",workCity="Central City",
+workState="MA",workCode="02104"
